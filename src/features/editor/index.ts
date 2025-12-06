43ad03b7b914ae572b2bc510df1d5ab54180b4f7
kimnamheeee <nami200284@snu.ac.kr>
2025-12-06 20:32:10 +0900

    🧹 chore(editor): remove unused files

--- src/features/editor/index.ts
+++ src/features/editor/index.ts
@@ -1,4 +1,3 @@
 export { default as ContentEditor } from "./ui/ContentEditor";
-export { default as ContentPreview } from "./ui/ContentPreview";
 export { default as PostTitleInput } from "./ui/PostTitleInput";
 export { useEditorStore } from "./model/editorStore";
