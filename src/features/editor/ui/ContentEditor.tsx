5f6d7ec369e9511d4b596aa292e3472644042bb6
kimnamheeee <nami200284@snu.ac.kr>
2025-12-06 21:54:05 +0900

    ✨ feat(write): add bubble menu to editor

--- src/features/editor/ui/ContentEditor.tsx
+++ src/features/editor/ui/ContentEditor.tsx
@@ -5,6 +5,9 @@ import { Placeholder } from "@tiptap/extensions";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
+import { BubbleMenu } from "@tiptap/react/menus";
+import { Button } from "@/shared/ui/button";
+import { ButtonGroup } from "@/shared/ui/button-group";
 
 import { useEditorStore } from "../model/editorStore";
 
@@ -25,5 +28,21 @@ export default function ContentEditor() {
     },
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <>
+      {editor && (
+        <BubbleMenu editor={editor}>
+          <ButtonGroup>
+            <Button variant="outline" onClick={() => {}}>
+              표현 다듬기
+            </Button>
+            <Button variant="outline" onClick={() => {}}>
+              AI에게 질문하기
+            </Button>
+          </ButtonGroup>
+        </BubbleMenu>
+      )}
+      <EditorContent editor={editor} />
+    </>
+  );
 }
