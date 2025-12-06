739f358ed73ddfbda9b65e9eae1f6630fa21c274
kimnamheeee <nami200284@snu.ac.kr>
2025-12-06 22:36:12 +0900

    💎 style(editor): adjust sidepanel layout

--- src/features/editor/ui/EditorSidePanel/EditorSidePanelTabs.tsx
+++ src/features/editor/ui/EditorSidePanel/EditorSidePanelTabs.tsx
@@ -5,13 +5,13 @@ import AiChatTab from "./TabContent/AiChatTab";
 
 export default function EditorSidePanel() {
   return (
-    <Tabs defaultValue="preview" className="h-full">
+    <Tabs defaultValue="preview" className="h-full px-8 py-6">
       <TabsList>
         <TabsTrigger value="preview">미리보기</TabsTrigger>
         <TabsTrigger value="external-search">외부 검색</TabsTrigger>
         <TabsTrigger value="ai-chat">AI chat</TabsTrigger>
       </TabsList>
-      <div className="px-8 py-6 overflow-y-auto">
+      <div className="overflow-y-auto">
         <TabsContent value="preview">
           <PreviewTab />
         </TabsContent>
