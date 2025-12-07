c0678823be533975343b06902c1f3810a16b3612
kimnamheeee <nami200284@snu.ac.kr>
2025-12-07 15:39:10 +0900

    🧹 chore(shared/ui): change directory

--- client/src/features/editor/ui/PostTitleInput.tsx
+++ client/src/features/editor/ui/PostTitleInput.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/shared/lib/utils";
+import { cn } from "@/shared/ui/lib/utils";
 import { useEditorStore } from "../model/editorStore";
 
 interface PostTitleInputProps {
